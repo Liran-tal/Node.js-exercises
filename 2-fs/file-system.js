909b7b724fbc711041f6682fe741047ff795b91e
Liran-tal <taliran44@gmail.com>
2022-01-09 13:41:26 +0200

    file-system.js *.3

--- 2-fs/file-system.js
+++ 2-fs/file-system.js
@@ -1,14 +1,17 @@
 const fs = require('fs');
 
-fs.writeFileSync('example.txt', 'example-1');
+// fs.writeFileSync('example.txt', 'example-1');
 
-fs.copyFile('./example.txt', './example-copy.txt', (err) => {
-  if (err) throw err;
-  console.log('source.txt was copied to destination.txt');
-});
+// fs.copyFile('./example.txt', './example-copy.txt', (err) => {
+//   if (err) throw err;
+//   console.log('./example1.txt was copied to ./example-copy.txt');
+// });
 
 
-fs.copyFile('./example-copy.txt', 'example2', (err) => {
-  if (err) throw err;
-  console.log('source.txt was copied to destination.txt');
-});
+// fs.rename('./example-copy.txt', 'example2.txt', (err) => {
+//   if (err) throw err;
+//   console.log('example-copy.txt was renamed to example2.txt');
+// });
+
+
+console.log(fs.readdirSync('./'));
