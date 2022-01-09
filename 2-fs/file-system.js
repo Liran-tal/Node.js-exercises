ec6eecc718a7271877821c2e60586531b0238e0c
Liran-tal <taliran44@gmail.com>
2022-01-09 13:28:19 +0200

    file-system.js *.2

--- 2-fs/file-system.js
+++ 2-fs/file-system.js
@@ -1,3 +1,14 @@
 const fs = require('fs');
 
 fs.writeFileSync('example.txt', 'example-1');
+
+fs.copyFile('./example.txt', './example-copy.txt', (err) => {
+  if (err) throw err;
+  console.log('source.txt was copied to destination.txt');
+});
+
+
+fs.copyFile('./example-copy.txt', 'example2', (err) => {
+  if (err) throw err;
+  console.log('source.txt was copied to destination.txt');
+});
