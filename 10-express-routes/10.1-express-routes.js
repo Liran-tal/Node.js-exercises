e6d3cf47ac60f2d1886a6e14cd86878ca676c712
Liran-tal <taliran44@gmail.com>
2022-01-16 18:41:06 +0200

    wtFFFFFF

--- 10-express-routes/10.1-express-routes.js
+++ 10-express-routes/10.1-express-routes.js
@@ -1,22 +1,46 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 const port = 8080;
+const numbers = [1,2,3,4,5,6];
 
 app.post('/numbers', (req, res) => {
-	res.send("success using post")
+	const { number } = req.body;
+	if (!numbers.includes(number)) {
+		numbers.push(number);
+		res.send(JSON.stringify(numbers));
+	}
+	else {
+		res.send("ERROR 400: Item already exist.")
+ 	}
 });
 
 app.get('/numbers', (req, res) => {
-	res.send("success using get")
+	res.send(JSON.stringify(numbers));
 });
 
 app.put('/numbers', (req, res) => {
-	res.send("success using put")
+	const { id } = req.query;
+	const { newNumber } = req.body;
+	if (numbers.includes(Number(id))) {
+		numbers.splice(numbers.indexOf(Number(id)), 1, newNumber);
+		res.send(JSON.stringify(numbers));
+	}
+	else {
+		 res.send("ERROR 400: Item does not exist.")
+	}
 });
 
 app.delete('/numbers', (req, res) => {
-	res.send("success using delete")
+	const { id } = req.query;
+	if (numbers.includes(Number(id))) {
+		numbers.splice(numbers.indexOf(Number(id)), 1);
+		res.send(JSON.stringify(numbers));
+	}
+	else {
+		 res.send("ERROR 400: Item does not exist.")
+	}
 });
 
 
